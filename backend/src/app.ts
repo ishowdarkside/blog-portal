1b71b3dad2f51fe4c6987ebe6d036b53fc21bd21
ishowdarkside <ajdin.omerovicc.ets@gmail.com>
2023-09-26 21:40:57 +0200

    connect to db

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import morgan from "morgan";
 const app = express();
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: path.join(__dirname, "config.env") });
+
+app.use(morgan("dev"));
+
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,20 @@
 import app from "./app";
+import mongoose from "mongoose";
+
+const DB = process.env.DB_LINK?.replace(
+  "<PASSWORD>",
+  <string>process.env.DB_PASSWORD
+);
+
+mongoose
+  .connect(<string>DB)
+  .then(() => {
+    console.log("Successfully connected to database!");
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
